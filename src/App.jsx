36dee689a6b5f14ebd4667ad405e6dc68f4c85ef
samuelgomeslima <samuelgomeslima@hotmail.com>
2025-10-02 16:18:24 -0300

    Implemented the retrieval of pizza images from the backend.

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,26 @@ const App = () => {
   return (
     <div>
       <h1>Padre Gino's - Order Now</h1>
-      <Pizza name="The Pepperoni Pizza" description="A delicious pizza topped with pepperoni slices and mozzarella cheese." />
-      <Pizza name="Americano Pizza" description="A classic pizza with tomato sauce, mozzarella cheese, and a variety of toppings." />
-      <Pizza name="Margherita Pizza" description="A simple yet flavorful pizza with fresh tomatoes, mozzarella cheese, basil, and olive oil." />
-      <Pizza name="Hawaiian Pizza" description="A tropical twist on pizza with ham, pineapple, and mozzarella cheese." />
-      <Pizza name="Veggie Pizza" description="A healthy option loaded with fresh vegetables like bell peppers, onions, mushrooms, and olives." />
+      <Pizza
+        name="The Pepperoni Pizza"
+        description="A delicious pizza topped with pepperoni slices and mozzarella cheese."
+        image={"/public/pizzas/pepperoni.webp"} />
+      <Pizza
+        name="Americano Pizza"
+        description="A classic pizza with tomato sauce, mozzarella cheese, and a variety of toppings."
+        image={"/public/pizzas/ckn_alfredo.webp"} />
+      <Pizza
+        name="Margherita Pizza"
+        description="A simple yet flavorful pizza with fresh tomatoes, mozzarella cheese, basil, and olive oil."
+        image={"/public/pizzas/ital_cpcllo.webp"} />
+      <Pizza
+        name="Hawaiian Pizza"
+        description="A tropical twist on pizza with ham, pineapple, and mozzarella cheese."
+        image={"/public/pizzas/hawaiian.webp"} />
+      <Pizza
+        name="Veggie Pizza"
+        description="A healthy option loaded with fresh vegetables like bell peppers, onions, mushrooms, and olives."
+        image={"/public/pizzas/veggie_veg.webp"} />
     </div>
   );
 };
