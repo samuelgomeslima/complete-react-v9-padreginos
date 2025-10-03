56ed25f9e19f269d57a35298e06dc693305a664a
samuelgomeslima <samuelgomeslima@hotmail.com>
2025-10-03 11:17:05 -0300

    Implemented the Order Pizza component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,12 @@
 import { createRoot } from "react-dom/client";
-import Pizza from "./Pizza";
+import Order from "./Order";
 
 /* eslint-disable no-undef */
 const App = () => {
   return (
     <div>
       <h1>Padre Gino's - Order Now</h1>
-      <Pizza
-        name="The Pepperoni Pizza"
-        description="A delicious pizza topped with pepperoni slices and mozzarella cheese."
-        image={"/public/pizzas/pepperoni.webp"} />
-      <Pizza
-        name="Americano Pizza"
-        description="A classic pizza with tomato sauce, mozzarella cheese, and a variety of toppings."
-        image={"/public/pizzas/ckn_alfredo.webp"} />
-      <Pizza
-        name="Margherita Pizza"
-        description="A simple yet flavorful pizza with fresh tomatoes, mozzarella cheese, basil, and olive oil."
-        image={"/public/pizzas/ital_cpcllo.webp"} />
-      <Pizza
-        name="Hawaiian Pizza"
-        description="A tropical twist on pizza with ham, pineapple, and mozzarella cheese."
-        image={"/public/pizzas/hawaiian.webp"} />
-      <Pizza
-        name="Veggie Pizza"
-        description="A healthy option loaded with fresh vegetables like bell peppers, onions, mushrooms, and olives."
-        image={"/public/pizzas/veggie_veg.webp"} />
+      <Order />
     </div>
   );
 };
--- src/Pizza.jsx
+++ src/Pizza.jsx
@@ -1,6 +1,11 @@
 const Pizza = (props) => {
     return (
-        <div className="pizza">
+        <div
+            className="pizza"
+            onClick={() => {
+                console.log("Hi");
+            }}
+        >
             <h1>{props.name}</h1>
             <p>{props.description}</p>
             <img src={props.image} alt={props.name} />
