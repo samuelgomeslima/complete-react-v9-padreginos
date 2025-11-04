ddbda036550114fc898d0b69f884b8c313b48775
samuelgomeslima <samuelgomeslima@hotmail.com>
2025-11-04 16:51:47 -0300

    Implemented Error Boundaries.

--- src/ErrorBoundary.jsx
+++ src/ErrorBoundary.jsx
@@ -3,11 +3,6 @@ import { Link } from "@tanstack/react-router";
 
 class ErrorBoundary extends Component {
     state = { hasError: false };
-    constructor (props) {
-        super (props);
-        
-        this.celebrateError = this.celebrateError.bind(this);
-    }
     static getDerivedStateFromError() {
         return { hasError: true };
     }
@@ -15,20 +10,11 @@ class ErrorBoundary extends Component {
     componentDidCatch(error, info) {
        console.error("ErrorBoundary caught an error", error, info);
     }
-    componentDidMount() {}
-    componentWillUnmount() {}
-    componentDidUpdate() {}
-    celebrateError = () => {
-        this.setState({
-            celebratinon: "lol"
-        });
-    }
-
+    
     render() {
-        console.log(this.props.lol);
         if (this.state.hasError) {
             return (
-                <div onClick={this.celebrateError} className="error-boundary">
+                <div className="error-boundary">
                     <h2>Something went wrong.</h2>
                     <p>
                         Please try refreshing the page or contact support if the problem persists. <Link to="/">Click here </Link> to go back to the home page.
