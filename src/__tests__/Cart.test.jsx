60f7c3ee942965b64e5eadd54c0174a36449e731
samuelgomeslima <samuelgomeslima@hotmail.com>
2025-11-13 11:46:48 -0300

    Implemented Snapshot Testing.

---
+++ src/__tests__/Cart.test.jsx
@@ -0,0 +1,31 @@
+import { expect, test } from 'vitest';
+import { render } from '@testing-library/react';
+import Cart from '../Cart';
+
+test("snapshot with nothing in cart", () => {
+  const { asFragment } = render(<Cart cart={[]} />);
+  expect(asFragment()).toMatchSnapshot();
+});
+
+
+test("snapshot with nothing in cart", () => {
+  const { asFragment } = render(<Cart cart={[]} />);
+  expect(asFragment()).toMatchInlineSnapshot(`
+    <DocumentFragment>
+      <div
+        class="cart"
+      >
+        <h2>
+          Cart
+        </h2>
+        <ul />
+        <p>
+          Total: $0.00
+        </p>
+        <button>
+          Checkout
+        </button>
+      </div>
+    </DocumentFragment>
+  `);
+});
